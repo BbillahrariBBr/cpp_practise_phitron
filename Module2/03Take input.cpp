abb8eea5fb629e8b9b520d529c857b381c802106
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-03 20:53:24 +0600

    take input in c

---
+++ Module2/03Take input.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char c[50];
+  cin.getline(c,50);
+  cout << c;
+  int a;
+  cin >> a;
+  cout << a << endl;
+  char b[5];
+  cin >> b;
+  cout <<b << endl;
+
+  return 0;
+}
+
