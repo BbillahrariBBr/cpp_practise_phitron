0567c4ed21a766d124483481b3b94089e6dd29a6
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-03 20:38:41 +0600

    float value print in cpp

---
+++ Module2/02 Float value print.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a = 10.456789;
+    printf("%.3f\n",a);
+    cout<<a<<endl;
+    cout<<fixed<<setprecision(3)<<a;
+
+  return 0;
+}
+
