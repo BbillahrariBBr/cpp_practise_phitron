6258c11ec0e9b3667cc292895ca92c44c20eb160
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-08 22:42:51 +0600

    check symmetric matrix

---
+++ Module4/4 check Symmetric matrix.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row, col, i,j;
+    cin >>row>>col;
+    int a[row][col];
+    for(i =0; i<row; i++)
+    {
+        for(j =0; j<col; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    int sum =0;
+    for(i =0; i<row; i++)
+    {
+        for(j =0; j<col; j++)
+        {
+           if(a[i][j]!= a[j][i])
+           {
+               cout<<"NO"<<endl;
+               return 0;
+           }
+        }
+    }
+    cout<<"YES"<<endl;
+
+  return 0;
+}
+
+
