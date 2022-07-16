0d884af1bb5310d185b9a49329d4c140e073fdf4
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-16 22:32:54 +0600

    encapsulation done

---
+++ Module5/02Encapsulations.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    int x;
+    void sayHi()
+    {
+        cout<<"Hi"<<endl;
+    }
+};
+
+int main()
+{
+    Student st;
+    st.x =10;
+
+
+  return 0;
+}
+
