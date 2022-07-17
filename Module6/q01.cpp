0be3185c423289d18993a50e0aeac508037d3af6
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-17 22:35:52 +0600

    string q1

---
+++ Module6/q01.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string s1="abc";
+
+    string s2="xyz";
+
+    s1=s2;
+
+    s2=s1;
+
+    cout<<s1<<" "<<s2<<endl;
+  return 0;
+}
+
