81a1b8b8db070964aa51c4337e7a5284f35fcebd
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-17 22:25:38 +0600

    strin operation

---
+++ Module6/06String_operation.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string st = "aaaabbbaaazz";
+    //find
+    cout<<st.find('z')<<endl;
+    cout<<st.find_first_of('z')<<endl;
+    //revers find
+    cout<<st.rfind('a')<<endl;
+    cout<<st.find_last_of('a')<<endl;
+    //not find
+    cout<<st.find_first_not_of('a')<<endl;
+    //not find
+    cout<<st.find_last_not_of('z')<<endl;
+    //substring
+    cout<<st.substr(3,5)<<endl;
+//    //count
+//    cout<<st.count('a')<<endl;
+
+
+  return 0;
+}
+
