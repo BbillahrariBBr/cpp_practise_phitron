d145ad3a7e8e4807e4b5568f7d75848bd8de946f
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-17 22:01:55 +0600

    strin element access

---
+++ Module6/04string_element_access.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string st =  "abcdefghhij";
+  cout<<st[3]<<endl;
+  cout<<st.at(3)<<endl;
+  cout<<st.front()<<endl;
+  cout<<st.back()<<endl;
+  return 0;
+}
+
