48a02ee264f090c06dba664b45891c14e2f8c0f3
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-17 22:42:12 +0600

    string q5

--- Module6/q05.cpp
+++ Module6/q05.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1="nortihP";
+
+    string:: reverse_iterator it;
+
+    for(it=s1.rbegin();it!=s1.rend();it++)
+
+     {
+
+         cout<<*it;
+
+     }
+
+  return 0;
+}
 
