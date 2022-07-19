c6d27be40dce2172140681d2be53cc291f30c5b8
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-19 21:56:51 +0600

    create account done

--- Module7/BankManagement.cpp
+++ Module7/BankManagement.cpp
@@ -20,11 +20,39 @@ public:
         this->age = age;
         this->password = password;
         this->account_number= rand()%1000000000;
-        cout<<"Your Account no is " <<
+        this->balace =0;
+        cout<<"Your Account no is " <<this->account_number<<endl;
+    }
+    int show_balance(string password){
+        if(this->password == password)
+        {
+            return this->balace;
+        }
+        else{
+            return -1;
+        }
     }
 };
+BankAccount* create_account()
+{
+    string account_holder,password,address;
+    int age;
+    cout<<"Create Account" <<endl;
+    cin>>account_holder>>address>>age>>password;
+    BankAccount *myAccount = new BankAccount(account_holder,address,age,password);
+    return myAccount;
+}
 int main()
 {
+
+  BankAccount *myAccount = create_account();
+  if(myAccount->show_balance("abc") == -1)
+  {
+      cout<< "Password didn't match "<<endl;
+  }
+  else{
+    cout<< "Your Bank balance is "<< myAccount->show_balance("abc")<<endl;
+  }
 
   return 0;
 }
