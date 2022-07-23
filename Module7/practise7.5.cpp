3ea3048a4d23f227880b92c9e0fa903309391679
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-23 21:34:34 +0600

    7.5 done

--- Module7/practise7.5.cpp
+++ Module7/practise7.5.cpp
@@ -3,9 +3,7 @@ using namespace std;
 class Shop
 {
 public:
-
-
-    string producut_name[10];
+    string product_name[10];
     int product_price[10];
     int product_quantity[10];
 
@@ -31,7 +29,66 @@ int main()
     cout<<"Number of Product: ";
     int n;
     cin>>n;
-    for(in)
+    for(int i=1; i<=n; i++)
+    {
+        cout<<"Product "<<i<<" Name: ";
+        cin>>myShop->product_name[i];
+        cout<<"Product "<<i<<" Price: ";
+        cin>>myShop->product_price[i];
+        cout<<"Product "<<i<<" Quantity: ";
+        cin>>myShop->product_quantity[i];
+    }
+    while(true)
+    {
+
+
+        // output
+        cout<<endl;
+        cout<<"\t\t Buy Product"<<endl;
+        cout<<"\t\t_____________"<<endl;
+        cout<<"Product Index\t\t";
+        for(int i =1; i<=n; i++)
+        {
+            cout<<i<<"\t";
+        }
+        cout<<endl;
+        cout<<"Product Name\t\t";
+        for(int i =1; i<=n; i++)
+        {
+            cout<<myShop->product_name[i]<<"\t";
+        }
+        cout<<endl;
+        cout<<"Product Price\t\t";
+        for(int i =1; i<=n; i++)
+        {
+            cout<<myShop->product_price[i]<<"\t";
+        }
+        cout<<endl;
+
+        cout<<"Product quantity\t";
+        for(int i =1; i<=n; i++)
+        {
+            cout<<myShop->product_quantity[i]<<"\t";
+        }
+        cout<<endl<<endl;
+
+        cout<<"Which Product do you want? From 1 to "<<n<<": ";
+        int indx, quantity;
+        cin>>indx;
+        cout<<"What is the quantity that you want of "<<myShop->product_name[indx]<<" : ";
+        cin>>quantity;
+        if(myShop->product_quantity[indx] <quantity)
+        {
+            cout<<"This quantity is not available"<<endl;
+        }
+        else{
+            myShop->set_total(quantity*myShop->product_price[indx]);
+            myShop->product_quantity[indx] -=quantity;
+            cout<<endl;
+            cout<<myShop->product_name[indx]<<" bought "<<quantity<<" px Successfully"<<endl;
+            cout<<"your total income : "<< myShop->get_total()<<endl;
+        }
+    }
 
   return 0;
 }
