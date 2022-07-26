26d349d8c08934347a171de0d60cc73b1cebd194
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-26 23:42:18 +0600

    bubble sort with out mod

---
+++ Module11/bubbolsort_1.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArray(int array[],int size)
+{
+    for(int i =0; i<size; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+void swap(int a, int b)
+{
+    int temp;
+    temp = a;
+    a= b;
+    b =a;
+}
+
+int main()
+{
+    int size;
+    cin>>size;
+    int array[size];
+
+    for(int i =0; i<size; i++)
+    {
+        cin>>array[i];
+    }
+    cout<<"Before sort : ";
+    printArray(array, size);
+    // bubble sort implementation
+
+    for(int i =0; i<size; i++)
+    {
+        for(int j =0; j<size-1; j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+
+    cout<<"After sort : ";
+    printArray(array, size);
+
+
+
+  return 0;
+}
+
