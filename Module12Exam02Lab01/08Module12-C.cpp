93174d8ef1b98f092adebc50a021ae1547f26324
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-30 10:18:49 +0600

    08Module12-1 lab xm done

---
+++ Module12Exam02Lab01/08Module12-C.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    int size = s.length();
+    int positionA;
+    int positionZ;
+    //cout<<size;
+    //for A position;
+    for(int i =0; i<size; i++)
+    {
+        if(s[i]=='A')
+        {
+            positionA = i+1;
+            break;
+        }
+    }
+
+    //for z  position;
+    for(int i =0; i<size; i++)
+    {
+        if(s[i]=='Z')
+        {
+            positionZ= i+1;
+        }
+    }
+
+    cout<<positionZ-positionA+1<<endl;
+
+
+
+  return 0;
+}
+
