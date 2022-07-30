abac73aba1d90b2c67fbb367a84d221f16e65159
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-30 22:04:25 +0600

    module 12 done

--- Module12Exam02Lab01/5Module12-1.cpp
+++ Module12Exam02Lab01/5Module12-1.cpp
@@ -5,7 +5,7 @@ void printArray(int array[], int size)
 {
     for(int i =1; i<size; i++)
     {
-        cout<<array[i]<<endl;
+        cout<<array[i]<<" ";
     }
     cout<<endl;
 }
@@ -19,72 +19,84 @@ void printArray(int array[], int size)
 
 int main()
 {
-    int size,q;
-    //cout<<"size and q: "<<endl;
-    cin>>size>>q;
-    int array[size+1];
-    array[0] =0;
-    //cout<<"array"<<endl;
-    for(int i=1; i<=size; i++)
+    int cas;
+    cin>>cas;
+    for(int c = 0; c<cas; c++)
     {
-        cin>>array[i];
-    }
-    int preArr[size+1] ={0};
-    for(int i =1; i<=size; i++)
-    {
-        preArr[i] =array[i]+preArr[i-1];
-    }
-    while(q--)
-    {
-        int input;
-        //cout<<"input"<<endl;
-        cin>>input;
-        if(input==1)
+        int size,q;
+        //cout<<"size and q: "<<endl;
+        cin>>size>>q;
+        int array[size+1];
+        array[0] =0;
+        //cout<<"array"<<endl;
+        for(int i=1; i<=size; i++)
         {
-            int pos;
-            //cout<<"pos"<<endl;
-            cin>>pos;
-            cout<<array[pos+1];
-            array[pos+1]= 0;
-            for(int i =pos+1; i<=size; i++)
-                {
-                    preArr[pos+1] =array[i]+preArr[i-1];
-                }
+            cin>>array[i];
         }
-        if(input==2)
+        int preArr[size+1] ={0};
+        for(int i =1; i<=size; i++)
         {
-            int pos,v;
-            //cout<<"pos and v"<<endl;
-            cin>>pos>>v;
-            array[pos+1]+=v;
-            //cout<<endl<<array[pos+1];
-            for(int i =pos+1; i<=size; i++)
-                {
-                    preArr[i] =array[i]+preArr[i-1];
-                }
-                //printArray(preArr,size+1);
+            preArr[i] =array[i]+preArr[i-1];
         }
-
-        if(input==3)
+        cout<<"Case "<<c+1<<":"<<endl;
+        while(q--)
         {
-            int pos,pos1;
-            //cout<<"pos and pos1"<<endl;
-            cin>>pos>>pos1;
-            if(pos==0)
+            int input;
+            //cout<<"input"<<endl;
+            cin>>input;
+            if(input==1)
             {
-                cout<<array[pos1+1]<<endl;
-            }
-            else{
-                cout<<preArr[pos1+1]-preArr[pos+1]<<endl;
-            }
-            //array[pos+1]+=v;
-            //cout<<endl<<array[pos+1];
+                int pos;
+                //cout<<"pos"<<endl;
+                cin>>pos;
+                cout<<array[pos+1]<<endl;
+                array[pos+1]= 0;
+                for(int i =pos+1; i<=size; i++)
+                    {
+                        preArr[i] =array[i]+preArr[i-1];
+                    }
+//                    printArray(array,size+1);
+//                     printArray(preArr,size+1);
 
-                //printArray(preArr,size+1);
+            }
+            if(input==2)
+            {
+                int pos,v;
+                //cout<<"pos and v"<<endl;
+                cin>>pos>>v;
+                array[pos+1]+=v;
+                for(int i =pos+1; i<=size; i++)
+                    {
+                        preArr[i] =array[i]+preArr[i-1];
+                    }
+//                    printArray(array,size+1);
+//                    printArray(preArr,size+1);
+
+            }
+
+            if(input==3)
+            {
+                int pos,pos1;
+                //cout<<"pos and pos1"<<endl;
+                cin>>pos>>pos1;
+                if(pos==0)
+                {
+                    cout<<preArr[pos1+1]<<endl;
+                }
+                else{
+                    cout<<preArr[pos1+1]-preArr[pos+1]<<endl;
+                }
+    //            array[pos+1]+=v;
+    //            cout<<endl<<array[pos+1];
+
+                    //printArray(preArr,size+1);
+            }
+
+
         }
 
-
     }
+
     //printArray(array,size+1);
     //printArray(preArr,size+1);
 
