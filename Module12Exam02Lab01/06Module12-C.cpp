ef57305876f281b98cf2ede91354454912fca3f6
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-30 11:20:28 +0600

    06-1Module12-1 lab xm done

---
+++ Module12Exam02Lab01/06Module12-C.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+//wrong 3 2 3
+int main()
+{
+    int array[3];
+    int count =0;
+    for(int i = 0; i<3; i++)
+    {
+        cin>>array[i];
+    }
+    for(int i =0; i<3-1; i++)
+    {
+       if(array[i] != array[i+1])
+        count++;
+    }
+    cout<<count+1<<endl;
+
+  return 0;
+}
+
