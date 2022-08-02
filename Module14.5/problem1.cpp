2e085985f057ce2551440d2c607bb7fbdffc3402
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-02 22:01:40 +0600

    problem 3 done

---
+++ Module14.5/problem1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArray(int *array, int row, int col)
+{
+    for(int i = 0; i<row; i++)
+    {
+        for(int j =0; j<col; j++)
+        {
+            cout << *((array+i*col)+j) << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    int array[row][col];
+
+    for(int i = 0; i<row; i++)
+    {
+        for(int j =0; j<col; j++)
+        {
+           array[i][j]=0;
+        }
+    }
+    printArray(*array,row,col);
+
+  return 0;
+}
+
