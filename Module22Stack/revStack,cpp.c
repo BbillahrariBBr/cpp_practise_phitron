120cfdbc8012843eaf7da721aced270706eeee51
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-17 00:45:00 +0600

    revers stack

---
+++ Module22Stack/revStack,cpp.c
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+#include "MYSTACK.h"
+using namespace std;
+
+void insertAtBottom(Stack<int>&chk, int chkElement)
+{
+    if(chk.empty())
+    {
+        chk.push(chkElement);
+        return;
+    }
+    int topElement = chk.topf();
+    chk.pop();
+    insertAtBottom(chk, chkElement)
+
+    chk.push(topElement);
+
+}
+void reverseStack(Stack<int> &chk)
+{
+    if(chk.empty())
+    {
+        return;
+    }
+    int topElement = chk.topf();
+    chk.pop();
+    reverseStack(chk);
+    insertAtBottom(chk, topElement);
+
+}
+int main()
+{
+    Stack <int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    reverseStack(st);
+
+  return 0;
+}
+
