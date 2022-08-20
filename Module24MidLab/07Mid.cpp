53b44d28e21dd9f27c4f395a437f0c05979c6df9
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-20 22:36:58 +0600

    mod 24 mid lab 10 mystack.h

---
+++ Module24MidLab/07Mid.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int value;
+    Node *next;
+
+    Node(int val)
+    {
+        this->value = val;
+        this->next = NULL;
+    }
+
+
+};
+
+Node* reverse(Node* head) {
+        if(head==NULL || head->next==NULL)
+            return head;
+
+        Node* temp=reverse(head->next);
+        Node* newHead=temp;
+        while(temp->next!=NULL)temp=temp->next;
+        temp->next=head;
+        head->next=NULL;
+        return newHead;
+    }
+
+
+
+Node* reverseKGroup(Node* head, int k) {
+        if(k==0 || head==NULL || head->next==NULL)
+            return head;
+
+        int cnt=k-1;
+        Node* tmp=head;
+        while(cnt && tmp->next!=NULL){
+            tmp=tmp->next;
+            cnt--;
+        }
+        if(cnt!=0)
+            return head;
+         Node* t=reverseKGroup(tmp->next,k);
+        tmp->next=NULL;
+         Node* newHead=reverse(head);
+        tmp=newHead;
+        while(tmp->next!=NULL)
+            tmp=tmp->next;
+        tmp->next=t;
+        return newHead;
+
+
+    }
+
+void insertLL(Node *&head, int value)
+{
+
+    Node *newNode = new Node(value);
+    if(head == NULL)
+    {
+        head=newNode;
+        return;
+    }
+    Node *temp = head;
+    while(temp->next != NULL)
+    {
+        temp= temp->next;
+    }
+    temp->next = newNode;
+}
+
+void display(Node *n)
+{
+    while(n!= NULL)
+    {
+        cout<<n->value;
+        if(n->next!= NULL)
+        {
+            cout<<"-> ";
+        }
+        n = n->next;
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    Node *head = NULL;
+    int n;
+    cin>>n;
+    int value;
+    for(int i = 0; i<n; i++)
+    {
+        cin>>value;
+//        if(value%2==0)
+//        {
+//            value = -1;
+//        }
+        insertLL(head,value);
+
+    }
+    int k;
+    cin >>k;
+    Node *newHead = reverseKGroup(head,k);
+
+    display(newHead);
+
+
+  return 0;
+}
+
+
---
+++ Module24MidLab/10Mid.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+#include"BAKIBILLAH_STACK.h"
+using namespace std;
+/*
+* stack of flot, stack of char, double, string, array, structure
+object of class
+*/
+int globalID = 100;
+class Person{
+    string name;
+    int id;
+    float salary;
+
+public:
+    //null Constractor
+    Person(){
+        name = "";
+        id = -1;
+        salary = -1.0;
+    }
+    void setName(string name)
+    {
+        this->name = name;
+    }
+
+    void setSalary(float salary)
+    {
+        this->salary = salary;
+    }
+    Person(string name, float salary){
+        //this->name = name;
+        setName(name);
+        //this->salary = salary;
+        setSalary(salary);
+        id = globalID;
+        globalID++;
+
+    }
+
+    int getId()
+    {
+        return id;
+    }
+    string getName()
+    {
+        return name;
+    }
+    float getSalary()
+    {
+        return salary;
+    }
+
+    void print()
+    {
+        cout<<name<<" | "<<id<<" | "<<salary <<endl;
+    }
+
+};
+int main()
+{
+    Stack <char> st;
+    st.push('a');
+    st.push('b');
+    st.push('c');
+    st.push('d');
+    st.push('e');
+//    char val = st.mid();
+    cout<<st.mid()<<endl;
+    st.push('f');
+    st.push('g');
+    cout<<st.mid()<<endl;
+    st.pop();
+    st.pop();
+    st.pop();
+    cout<<st.mid()<<endl;
+
+//    Stack <pair<int,char>> st;
+//    st.push(make_pair(1,'a'));
+//    st.push(make_pair(5,'d'));
+//    st.push(make_pair(7,'z'));
+
+//    while(!st.empty())
+//    {
+//        cout<<st.pop()<<endl;
+//    }
+//    pair <int,char> chk;
+//    chk = st.pop();
+//    cout<<chk.first<<endl;
+//    cout<<chk.second<<endl;
+//    cout<<st.size()<<endl;
+//
+//    pair <int,char> chk1;
+//    chk1 = st.topf();
+//    cout<<chk1.first<<endl;
+//    cout<<chk1.second<<endl;
+
+
+//
+//    Stack <Person> st;
+//    Person a ("Baki",654.36);
+//    Person b ("Billa",254.36);
+//    Person c ("Rari",64.36);
+//    st.push(a);
+//    st.push(b);
+//    st.push(c);
+//    Person pMid = st.mid();
+//    pMid.print();
+//    while(!st.empty())
+//    {
+//        Person printObj;
+//        printObj = st.pop();
+//        printObj.print();
+//    }
+//    while(!st.empty())
+//    {
+//        Person printObj;
+//        printObj = st.pop();
+//        printObj.print();
+//    }
+  return 0;
+}
+
+
