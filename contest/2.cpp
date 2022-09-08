41972a7ecaa5d01035a4f186a2221f0d71563a0c
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-08 20:44:55 +0600

    contest 2 done

---
+++ contest/2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    int missing_number;
+    while(test_case--)
+    {
+        int sum,a,b,c;
+        cin>>sum>>a>>b>>c;
+        missing_number = sum-(a+b+c);
+        cout<<missing_number<<endl;
+
+    }
+//    int sum,a,b,c;
+//    cin>>sum>>a>>b>>c;
+//    missing_number = sum-(a+b+c);
+//    cout<<missing_number<<endl;
+
+  return 0;
+}
+
