9b393adfcc21844882ef4d28e72be1df424c362c
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-25 19:50:12 +0600

    7 kamals neighbour done

---
+++ LabFinal/07.kamal'sNeughbour.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Graph{
+    int V;
+    list<int> *adj;
+public:
+    Graph(int V)
+    {
+        this->V=V;
+        adj = new list<int>[V];
+    }
+    void addEdge(int n, int e)
+    {
+        adj[n].push_back(e);
+        adj[e].push_back(n);
+    }
+
+    void printNeighbour(int chk)
+    {
+        //cout<<chk<<":";
+        int count =0;
+        for(auto i = adj[chk].begin(); i!=adj[chk].end(); i++)
+        {
+            //cout<<"( "<<(*i);
+            count++;
+        }
+        cout<<count<<endl;
+    }
+};
+
+int main()
+{
+    int N,E;
+    cin>>N>>E;
+    Graph g(N);
+    for(int i =0; i<E; i++)
+    {
+        //n = node e = edge w = weight
+        int A,B;
+        cin>>A>>B;
+        g.addEdge(A,B);
+    }
+    int k;
+    cin>>k;
+    g.printNeighbour(k);
+        cout<<endl;
+
+
+  return 0;
+}
+
